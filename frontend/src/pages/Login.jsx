0008c85f098c958525b65aa93eeea7d4fe62df61
maurogimenezb <maurogimenezb@gmail.com>
2023-05-08 13:02:15 -0400

    Improved responsiveness of Login page: centered content, adjusted title size, reduced space, and organized login buttons.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,13 +2,14 @@ const Login = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Bienvenido al Remote Dev Guru</h1>
-          <p className="py-6">
-            Para comenzar a chatear, inicie sesion
+        <div className="max-w-md mx-auto">
+          <h1 className="text-3xl md:text-5xl font-bold">Bienvenido al Remote Dev Guru</h1>
+          <p className="py-4">
+            Para comenzar a chatear, inicie sesión
           </p>
-          <button className="btn">Continuar con Google</button>
-          <button className="btn">Continuar con Facebook</button>
+          <div className="flex flex-col gap-4">
+            <button className="btn">Continuar con Google</button>
+          </div>
         </div>
       </div>
     </div>
@@ -16,3 +17,4 @@ const Login = () => {
 };
 
 export default Login;
+
