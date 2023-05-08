1b3e21a3794f4f4d44ab3bf1ceeeffa412c2677d
maurogimenezb <maurogimenezb@gmail.com>
2023-05-08 13:14:31 -0400

    Improved responsiveness of History page: centered content, adjusted title size, reduced space, and organized history buttons.

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -10,15 +10,15 @@ const History = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Historial de Conversaciones</h1>
-          <p className="py-6">
-          </p>
-          <Button text="Historial 1" />
-          <Button text="Historial 2" />
-          <Button text="Historial 3" />
-          <Button text="Historial 4" />
-          <Button text="Historial 5" />
+        <div className="max-w-md mx-auto">
+          <h1 className="text-3xl md:text-5xl font-bold mb-10">Historial de Conversaciones</h1> {/* Se agregó mb-6 para separar el título de los botones */}
+          <div className="flex flex-col gap-4">
+            <Button text="Historial 1" />
+            <Button text="Historial 2" />
+            <Button text="Historial 3" />
+            <Button text="Historial 4" />
+            <Button text="Historial 5" />
+          </div>
         </div>
       </div>
     </div>
@@ -26,3 +26,4 @@ const History = () => {
 };
 
 export default History;
+
