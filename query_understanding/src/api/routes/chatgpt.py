5d103ac2171764ebe5072e6373eba19a8bd77697
Mobin <Megamobin@gmail.com>
2023-05-14 17:02:01 -0400

    Agregado archivo chatgpt.py, que contiene un pequeno programa que llama a chatgpt mediante su API, solo queda conectar este modulo con el endpoint chat

--- query_understanding/src/api/routes/chatgpt.py
+++ query_understanding/src/api/routes/chatgpt.py
@@ -10,12 +10,23 @@ messages = [{"role":"system",
              "content":"Eres un asistente para los nuevos programadores que quieren empezar a trabajar en remoto. Les ayudaras respondiendo todas las preguntas que tienen. Te llamas DevGuru y siempre saludas cuando comienzas la conversacion."}
             ]
 
-content = "dime que necesito para empezar a programar en remoto"
+respuesta = False
+while respuesta == False:
+    content = "dime que necesito para empezar a programar en remoto"
 
-messages.append({"role":"user", "content":content})
+    if content == "exit":
+        break
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", messages=messages
-)
+    messages.append({"role":"user", "content":content})
 
-print(response.choices[0].message.content)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=messages)
+    
+    response_content = response.choices[0].message.content
+    
+    messages.append({"role":"assistant", "content":response})
+
+    print(response.choices[0].message.content)
+    
+    input()
+    respuesta = True
