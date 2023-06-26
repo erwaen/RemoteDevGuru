3a476a306db54a796e99ede19b158ead65674196
0-ifrit <56732944+0-ifrit@users.noreply.github.com>
2023-06-26 00:00:22 -0400

    Added a lifespan function to initialize the vector database before the FastAPI app starts.

--- retrieval_augmentation/api_script.py
+++ retrieval_augmentation/api_script.py
@@ -2,18 +2,52 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from getpass import getpass
+
 class Query(BaseModel):
     query_content: str
 
+docsearch = object()
+
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def initialize_vector_db(app: FastAPI):
+    # Get the Pinecone API key.
+    PINECONE_API_KEY = getpass("Pinecone API Key: ")
+    # Get the Pinecone environment (found in the API section).
+    PINECONE_ENV = input("Pinecone environment: ")
+
+    # Initilize a Python "instance" of Pinecone.
+    pinecone.init(
+        api_key=PINECONE_API_KEY,
+        environment=PINECONE_ENV
+    )
+
+    # Get OpenAI API key.
+    OPENAI_API_KEY = getpass("OpenAI API Key: ")
+    model = 'text-embedding-ada-002'
+
+    # Set the text embedding model. We will use one from OpenAI.
+    embeddings = OpenAIEmbeddings(
+        model=model,
+        openai_api_key=OPENAI_API_KEY
+    )
+
+    # Set the name for the new Pinecone index.
+    index_name = 'test'
+
+    docsearch = Pinecone.from_existing_index(
+        index_name, embeddings, text_key='text')
+
+    yield
 
 
+app = FastAPI(lifespan=initialize_vector_db)
 
-app = FastAPI(lifespan=lifespan)
-
-app = FastAPI()
+#app = FastAPI()
 
 @app.post("/")
 async def process_query(query: Query):
-    return query
+    return docsearch.similarity_search(query.query_content)
