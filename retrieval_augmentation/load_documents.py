e532d1a22bee9eb3971f1a8ee0e539ce88860b9e
0-ifrit <56732944+0-ifrit@users.noreply.github.com>
2023-06-26 02:39:19 -0400

    Added a second endpoint to the API for the Q&A functionality. Deleted unnecesary scripts.

--- retrieval_augmentation/load_documents.py
+++ retrieval_augmentation/load_documents.py
@@ -1,12 +1,13 @@
 import sys
-import os
 import pinecone
+import tiktoken
 from langchain.document_loaders import JSONLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from getpass import getpass
 
+# Make sure that the user inputs a directory name as argument.
 if len(sys.argv) < 2:
     print(
         "You must provide the directory in which the documents"\
@@ -14,32 +15,42 @@ if len(sys.argv) < 2:
     exit()
 
 # Get the name of the directory in which the JSON files are stored.
-directory = sys.argv[1]
+json_doc = sys.argv[1]
 
-# List every JSON document in the directory.
-directory = './linkedin_docs'
-json_docs = []
-for entry in os.listdir(directory):
-    path = os.path.join(directory, entry)
-    if os.path.isfile(path):
-        json_docs.append(path)
-
-linked_in = input("Is the data from linked in? Enter Y (yes) or N (no): ")
+# If the data is scrapped from LinkedIn, we have to embed the
+# "detalle_del_puesto" field. If it is from a website, we embed
+# the "content".
+linked_in = input("Is the data from linkedIn? Enter Y (yes) or N (no): ")
 jq_schema = '.[].content'
-if linked_in.lower() == 'y':
+if linked_in.lower() == 'y' or linked_in.lower() == 'yes':
     jq_schema='.[].detalle_del_puesto'
 
-# Load the JSON documents stored in the file system.
-for json_doc in json_docs:
-    loader = JSONLoader(
-        file_path=json_doc,
-        jq_schema=jq_schema)
-
+# Load the JSON document stored in the file system.
+loader = JSONLoader(file_path=json_doc, jq_schema=jq_schema)
 documents = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=0)
+# Set the tokenizer
+tokenizer = tiktoken.get_encoding('cl100k_base')
 
+# Create a length function to know the amount of tokens of a given
+# chunk of text.
+def tiktoken_len(text):
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+# Set the text splitter to separate a large documents into smaller
+# chunks, because LLMs accept a limited amount of tokens.
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=20,  # number of tokens overlap between chunks
+    length_function=tiktoken_len,
+    separators=['\n\n', '\n', ' ', '']
+)
+
+# Split the documents into smaller chunks of text.
 docs = text_splitter.split_documents(documents)
 
 # Get the Pinecone API key.
@@ -47,11 +58,11 @@ PINECONE_API_KEY = getpass("Pinecone API Key: ")
 # Get the Pinecone environment (found in the API section).
 PINECONE_ENV = input("Pinecone environment: ")
 
-# Set the name for the new Pinecone index.
-index_name = 'test'
-
-# Select the given Pinecone index.
-index = pinecone.Index(index_name)
+# Initilize a Python "instance" of Pinecone.
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENV
+)
 
 # Get OpenAI API key.
 OPENAI_API_KEY = getpass("OpenAI API Key: ")
@@ -63,9 +74,12 @@ embeddings = OpenAIEmbeddings(
     openai_api_key=OPENAI_API_KEY
 )
 
+# Set the name for the new Pinecone index.
+index_name = 'remote-dev-guru'
+
 Pinecone.from_documents(docs, embeddings, index_name=index_name)
 
 print(
     f"All of the documents from the directory {directory}"\
-    " have been embedded and added to the Pinecone"\
-    " index {index_name}.")
+    f" have been embedded and added to the Pinecone"\
+    f" index {index_name}.")
