90962372ff359d08c3554b57946b459f16b98219
0-ifrit <56732944+0-ifrit@users.noreply.github.com>
2023-06-26 00:40:28 -0400

    Fixed a bug.

--- retrieval_augmentation/api_script.py
+++ retrieval_augmentation/api_script.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from contextlib import asynccontextmanager
-
 import pinecone
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -10,43 +8,34 @@ from getpass import getpass
 class Query(BaseModel):
     query_content: str
 
-docsearch = object()
+# Get the Pinecone API key.
+PINECONE_API_KEY = getpass("Pinecone API Key: ")
+# Get the Pinecone environment (found in the API section).
+PINECONE_ENV = input("Pinecone environment: ")
 
-@asynccontextmanager
-async def initialize_vector_db(app: FastAPI):
-    # Get the Pinecone API key.
-    PINECONE_API_KEY = getpass("Pinecone API Key: ")
-    # Get the Pinecone environment (found in the API section).
-    PINECONE_ENV = input("Pinecone environment: ")
+# Initilize a Python "instance" of Pinecone.
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENV
+)
 
-    # Initilize a Python "instance" of Pinecone.
-    pinecone.init(
-        api_key=PINECONE_API_KEY,
-        environment=PINECONE_ENV
-    )
+# Get OpenAI API key.
+OPENAI_API_KEY = getpass("OpenAI API Key: ")
+model = 'text-embedding-ada-002'
 
-    # Get OpenAI API key.
-    OPENAI_API_KEY = getpass("OpenAI API Key: ")
-    model = 'text-embedding-ada-002'
+# Set the text embedding model. We will use one from OpenAI.
+embeddings = OpenAIEmbeddings(
+    model=model,
+    openai_api_key=OPENAI_API_KEY
+)
 
-    # Set the text embedding model. We will use one from OpenAI.
-    embeddings = OpenAIEmbeddings(
-        model=model,
-        openai_api_key=OPENAI_API_KEY
-    )
+# Set the name for the new Pinecone index.
+index_name = 'test'
 
-    # Set the name for the new Pinecone index.
-    index_name = 'test'
+docsearch = Pinecone.from_existing_index(
+    index_name, embeddings, text_key='text')
 
-    docsearch = Pinecone.from_existing_index(
-        index_name, embeddings, text_key='text')
-
-    yield
-
-
-app = FastAPI(lifespan=initialize_vector_db)
-
-#app = FastAPI()
+app = FastAPI()
 
 @app.post("/")
 async def process_query(query: Query):
