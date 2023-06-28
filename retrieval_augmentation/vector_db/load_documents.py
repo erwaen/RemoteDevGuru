6c03f2ff8e1b52f00380d00d49b496ecfa844234
0-ifrit <56732944+0-ifrit@users.noreply.github.com>
2023-06-28 06:16:44 -0400

    Fixed the bug related to the wrong information on the "source" field in the metadata of the entries in the vector database.

--- retrieval_augmentation/vector_db/load_documents.py
+++ retrieval_augmentation/vector_db/load_documents.py
@@ -3,17 +3,22 @@ import os
 import pinecone
 import tiktoken
 from langchain.document_loaders import JSONLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from dotenv import load_dotenv
+from aux_methods import (
+    user_input_to_bool,
+    metadata_fun_linkedin,
+    metadata_fun_web
+    )
 
 # Make sure that the user inputs a file name as argument.
 if len(sys.argv) < 2:
     print("You must provide a file from which to extract the data.")
     exit()
 
-# Get the name of the directory in which the JSON files are stored.
+# Get the name of the JSON file passed as argument to the script.
 json_doc = sys.argv[1]
 
 # Load the variables from the .env file as environment variables.
@@ -22,13 +27,19 @@ load_dotenv()
 # If the data is scrapped from LinkedIn, we have to embed the
 # "detalle_del_puesto" field. If it is from a website, we embed
 # the "content" field.
-linked_in = input("Is the data from LinkedIn? Enter Y (yes) or N (no): ")
-jq_schema = '.[].content'
-if linked_in.lower() == 'y' or linked_in.lower() == 'yes':
-    jq_schema='.[].detalle_del_puesto'
+linked_in = user_input_to_bool(
+    "Is the data from LinkedIn? Enter Y (yes) or N (no): ")
+content_key = 'detalle_del_puesto' if linked_in else 'content'
 
 # Load the JSON document stored in the file system.
-loader = JSONLoader(file_path=json_doc, jq_schema=jq_schema)
+loader = JSONLoader(
+    file_path=json_doc,
+    jq_schema='.[]',
+    content_key=content_key,
+    # The scrapper for LinkedIn is different from the one used for
+    # scrapping data from other websites.
+    metadata_func=metadata_fun_linkedin if linked_in else metadata_fun_web
+    )
 documents = loader.load()
 
 # Set the tokenizer
@@ -78,6 +89,6 @@ Pinecone.from_documents(docs, embeddings, index_name=index_name)
 
 # Print a success message for the user.
 print(
-    f"All of the documents from the directory {directory}"\
+    f"All of the documents from the directory {json_doc}"\
     f" have been embedded and added to the Pinecone"\
     f" index {index_name}.")
